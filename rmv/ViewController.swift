3623c48fbe2ff61f54d4f1fc6dd4652cc230ed62
shea <401603604@qq.com>
2016-08-23 14:02:39 +0800

    swift
    swift

---
+++ rmv/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rmv
+//
+//  Created by rm on 16/8/23.
+//  Copyright © 2016年 RongMei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
